c6e8068e3b45f8c3b25bf1be95c5804d2d29132c
Stella Sparkles <twinkle@mouse.lgbt>
2025-09-06 21:30:16 +0200

    restart nginx-index service in script

--- server-copy-and-build.sh
+++ server-copy-and-build.sh
@@ -5,3 +5,4 @@ mkdir -p /usr/local/etc/nginx-indexing/ || exit
 cp -r ./* /usr/local/etc/nginx-indexing/ || exit
 cd /usr/local/etc/nginx-indexing/ || exit
 ROOT_DIR=/mnt/shared/public/ SITE_CONFIG=Files_TheSqkyOne pnpm run build || exit
+systemctl restart nginx-index.service
