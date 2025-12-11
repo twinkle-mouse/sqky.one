8557b8e8974f31da598517a4009440eb0e69e8b4
Stella Sparkles <twinkle@mouse.lgbt>
2025-12-11 20:09:54 +0100

    visuals update, fix a few visual errors, move a few things, ran formatting

--- src/sites/writing.sqky.one/astro.config.ts
+++ src/sites/writing.sqky.one/astro.config.ts
@@ -1,7 +1,8 @@
 // @ts-check
 import mdx from "@astrojs/mdx";
 import { defineConfig } from "astro/config";
-import remarkSectionize from "remark-sectionize";
+
+import remarkSectionize from "./remark-sectionize";
 
 // https://astro.build/config
 export default defineConfig({
