101cb4db282bba64196b31b62bda4f188ae9dd63
Stella Sparkles <twinkle@mouse.lgbt>
2025-12-11 20:10:44 +0100

    deps, add rss

---
+++ src/sites/writing.sqky.one/src/pages/rss.xml.ts
@@ -0,0 +1,42 @@
+import mdx from "@astrojs/mdx/server.js";
+import rss from "@astrojs/rss";
+import type { AstroGlobal } from "astro";
+import { experimental_AstroContainer } from "astro/container";
+import { getCollection } from "astro:content";
+import { render } from "astro:content";
+import sanitizeHtml from "sanitize-html";
+
+import { defaultDescription, siteDesc, siteName } from "$lib/writings";
+
+const container = await experimental_AstroContainer.create();
+container.addServerRenderer({ renderer: mdx });
+
+export async function GET(context: AstroGlobal) {
+    const writings = await getCollection("writings");
+
+    return rss({
+        title: siteName,
+        site: context.site!,
+        description: siteDesc,
+        items: await Promise.all(
+            writings.map(async (post) => {
+                const { Content } = await render(post);
+                const content = await container.renderToString(Content, {
+                    props: { components: [] },
+                });
+
+                return {
+                    title: post.data.title,
+                    description: post.data.description || defaultDescription,
+                    pubDate: post.data.date,
+                    author: post.data.authors.join(", "),
+                    categories: post.data.tags,
+                    link: `/writings/${post.id}/`,
+                    content: sanitizeHtml(content.trim(), {
+                        allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+                    }),
+                };
+            }),
+        ),
+    });
+}
