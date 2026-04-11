0c64933fbdc0697e079a1091edee7ee55b754374
Stella Sparkles <twinkle@mouse.lgbt>
2026-04-12 01:49:28 +0200

    refactor, clean up

--- src/lib/writings.ts
+++ src/lib/writings.ts
@@ -1,4 +1,18 @@
-export const defaultDescription = "(no description)";
+import { getContainerRenderer } from "@astrojs/mdx";
+import { count } from "@wordpress/wordcount";
+import { experimental_AstroContainer } from "astro/container";
+import type { AstroComponentFactory } from "astro/runtime/server/index.js";
+import { loadRenderers } from "astro:container";
+
+const renderers = await loadRenderers([getContainerRenderer()]);
+const container = await experimental_AstroContainer.create({ renderers });
+
 export const siteName = "Books of Squeaks";
 export const siteDesc =
     "A place where I put all of my writings, or at least the ones I care enough to format and upload <3. Expect lots of emotions and vulnerability, and expect a hot personality.";
+
+export async function countWords(Content: AstroComponentFactory) {
+    const body = await container.renderToString(Content);
+
+    return count(body, "words", {});
+}
