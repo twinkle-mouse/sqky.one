ff0738ac9366988cb9ed4d02aaf8d48f788d728e
Stella Sparkles <twinkle@mouse.lgbt>
2026-04-13 15:44:44 +0200

    improve shadow

--- src/lib/colors.ts
+++ src/lib/colors.ts
@@ -191,8 +191,8 @@ export const semanticColorsDark = {
 
 export const semanticUtilsDark = {
     cardBoxShadow: `
-        0px 0px 6px 3px rgba(0, 0, 0, 0.5),
-        inset 0px 0px 3px 2px rgba(90, 90, 160, 0.2)
+        0px 0px 6px 4px rgba(0, 0, 0, 0.5),
+        inset 0px 0px 4px 3px rgba(60, 60, 90, 0.3)
     `,
 };
 
