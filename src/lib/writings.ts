e29ddea6845241311d6e364f3a36857d040bd58c
Stella Sparkles <twinkle@sqky.one>
2026-07-13 16:47:17 +0200

    fix some writing site issue with markdown/html parsing/rendering, parse html as text content first before counting the words

--- src/lib/writings.ts
+++ src/lib/writings.ts
@@ -4,13 +4,13 @@ import { experimental_AstroContainer } from "astro/container";
 import type { AstroComponentFactory } from "astro/runtime/server/index.js";
 import { loadRenderers } from "astro:container";
 import { type CollectionEntry, getCollection } from "astro:content";
-import { parse } from "node-html-parser";
+import { Node, NodeType, parse } from "node-html-parser";
 
 import { markdownProcessor } from "../sites/writing.sqky.one/astro.config";
 
 const renderers = await loadRenderers([getContainerRenderer()]);
 const container = await experimental_AstroContainer.create({ renderers });
-const mdRenderer = await markdownProcessor.createRenderer(markdownProcessor.options);
+const mdRenderer = await markdownProcessor.createRenderer({});
 
 export const siteName = "Stardew Library";
 export const siteDesc =
@@ -18,17 +18,65 @@ export const siteDesc =
 
 const defaultDescription = "(no description)";
 
+function flattenTreeDepthFirst(rootNode: Node) {
+    const nodes = [];
+    const queue: Node[][] = [];
+
+    if (rootNode.childNodes) {
+        queue.push(rootNode.childNodes);
+    }
+
+    while (queue.length > 0) {
+        const children = queue[0];
+        if (children.length > 0) {
+            const child = children.shift()!;
+            if (children.length < 1) {
+                queue.shift();
+            }
+            if (child.childNodes && child.childNodes.length > 0) {
+                queue.unshift(child.childNodes);
+            }
+            nodes.push(child);
+        }
+    }
+
+    return nodes;
+}
+
 export async function renderMd(content: string | undefined | null) {
-    return (await mdRenderer.render(content ?? "")).code.replaceAll("\n", "").replaceAll("\r", "");
+    return (await mdRenderer.render(content ?? "")).code
+        .trim()
+        .replaceAll("&lt;", "<") // unescape
+        .replaceAll("&gt;", ">")
+        .replaceAll("\r", "") // remove \r
+        .replaceAll("\n", " "); // replace \n with a space, it wouldn't render in HTML but it can be used in `htmlToTextContent`;
 }
 
 export function htmlToTextContent(content: string | undefined | null) {
     const element = parse(content ?? "");
-    return element.textContent;
+    const flatChilren = flattenTreeDepthFirst(element);
+
+    let text = "";
+    for (const child of flatChilren) {
+        if (child.rawTagName == "br") {
+            text += "\n";
+            continue;
+        }
+
+        if (child.nodeType == NodeType.TEXT_NODE) {
+            text += child.textContent;
+        }
+
+        if (child.rawTagName == "p") {
+            text += "\n\n";
+        }
+    }
+
+    return text.trim();
 }
 
 export async function countWords(Content: AstroComponentFactory) {
-    const body = await container.renderToString(Content);
+    const body = htmlToTextContent(await container.renderToString(Content));
 
     return count(body, "words", {});
 }
