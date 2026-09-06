5de97857c5b1c3f118feb2822362d442a4a330ae
Stella Sparkles <twinkle@sqky.one>
2026-09-06 02:34:49 +0200

    add radio input component

--- common/assets/Icons.ts
+++ common/assets/Icons.ts
@@ -1,7 +1,8 @@
 import BackToTop from "./icons/back-to-top.svg";
 import CaretRight from "./icons/caret-right.svg";
 import Cross from "./icons/cross.svg";
+import Dot from "./icons/dot.svg";
 import TFHazard from "./icons/tf-hazard.png";
 import ThetaDelta from "./icons/theta-delta.png";
 
-export { TFHazard, ThetaDelta, BackToTop, CaretRight, Cross };
+export { TFHazard, ThetaDelta, BackToTop, CaretRight, Cross, Dot };
